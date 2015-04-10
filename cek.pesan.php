bf984479d72eddc82d3d833600b56b93d260b7da
Hendra Randy Nomura <hendra.randy.nomura@gmail.com>
2015-04-11 00:26:25 +0700

    Update cek.pesan.php

--- cek.pesan.php
+++ cek.pesan.php
@@ -7,6 +7,7 @@ $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 if($_POST[tujuan]=="send"){
 	$qt="insert into isi_pesan values(null,'$_SESSION[username]','$_POST[kepada]','$_POST[pesan]',CURRENT_TIMESTAMP,0,0)";$tmp=$pdo->prepare($qt);$tmp->execute();
 	echo"<div class='lchat'>$_POST[pesan]</div><div class='clear'></div>";
+	
 }elseif($_POST[tujuan]=="cek"){
 	$qt="select * from isi_pesan where penerima='$_SESSION[username]' and pengirim='$_POST[id]' and dibaca=0";$tmp=$pdo->prepare($qt);$tmp->execute();$cek=$tmp->fetch(PDO::FETCH_NUM);
 	if($cek>0){
@@ -27,18 +28,28 @@ if($_POST[tujuan]=="send"){
 	foreach($pdo->query($load) as $r){
 		if(($r[pengirim]=="$_SESSION[username]")&&($r[penerima]=="$_POST[user]")){
 			echo"<div class='lchat'>$r[isi_pesan]</div><div class='clear'></div>";
+			$bc="update isi_pesan set dibaca=1,tampil=1 where id_pesan='$r[id_pesan]'";$ekse=$pdo->prepare($bc);$ekse->execute();
 		}elseif(($r[pengirim]=="$_POST[user]")&&($r[penerima]=="$_SESSION[username]")){
 			echo"<div class='rchat'>$r[isi_pesan]</div><div class='clear'></div>";
+			$bc="update isi_pesan set dibaca=1,tampil=1 where id_pesan='$r[id_pesan]'";$ekse=$pdo->prepare($bc);$ekse->execute();
 		}
 	}
-	$bc="update isi_pesan set dibaca=1,tampil=1 where penerima='$_SESSION[username]' and pengirim='$_POST[user]'";$ekse=$pdo->prepare($bc);$ekse->execute();
+	
 }elseif($_POST[tujuan]=="autocek"){
 	$load="select isi_pesan,penerima,pengirim,id_pesan from isi_pesan where dibaca=0 group by id_pesan";
 	foreach($pdo->query($load) as $r){
 		if(($r[pengirim]=="$_POST[user]")&&($r[penerima]=="$_SESSION[username]")){
 			echo"<div class='rchat'>$r[isi_pesan]</div><div class='clear'></div>";
+			$bc="update isi_pesan set dibaca=1,tampil=1 where id_pesan='$r[id_pesan]'";$ekse=$pdo->prepare($bc);$ekse->execute();
 		}
 	}
-	$bc="update isi_pesan set dibaca=1,tampil=1 where penerima='$_SESSION[username]' and pengirim='$_POST[user]'";$ekse=$pdo->prepare($bc);$ekse->execute();
+}elseif($_POST[tujuan]=="cek3"){ //cek jika ada pesan belum dibaca
+	$query="select a.pengirim from isi_pesan a,pesan b where a.penerima='$_SESSION[username]' and a.penerima=b.user1 and b.tampil=0 and a.dibaca=0 group by a.id_pesan";$pre=$pdo->prepare($query);$pre->execute();$cek=(int) $pre->fetch(PDO::FETCH_NUM);
+	if($cek>0){
+		$query="select a.pengirim from isi_pesan a,pesan b where a.penerima='$_SESSION[username]' and a.penerima=b.user1 and b.tampil=0 and a.dibaca=0 group by a.id_pesan";$pre=$pdo->prepare($query);$pre->execute();$r=$pre->fetch(PDO::FETCH_ASSOC);
+		echo"$r[pengirim]";
+	}else{
+		echo 0;
+	}
 }
 ?>
